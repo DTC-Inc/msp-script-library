113bf6332441f849b9e6ec4217708022e3ca780a
Nate Smith <nate.smith@dtctoday.com>
2023-05-27 00:52:41 -0400

    Update veeam-add-backup-repo.ps1

--- veeam/veeam-add-backup-repo.ps1
+++ veeam/veeam-add-backup-repo.ps1
@@ -2,26 +2,24 @@
 # S3 Compabitble & Local
 
 Start-Transcript 
+# Set the repository details
 
 $repositoryType = "Please enter the repository type (1 S3; 2 Local)"
-
 $description = Read-Host "Please enter the ticket # or project ticket # related to this configuration" | Out-String
-
 $immutabilityPeriod = Read-Host "Enter how many days every object is immutable for"
-
-# Set the S3-compatible repository details
-$repositoryName = "backblaze provided by dtc"
-$accessKey = "0027d5671c7a12400000000c4"
-$secretKey = "K002x7G9RGlnlpnwp4TFMO19jafnfQ4"
-$endpoint = "https://s3.us-west-002.backblazeb2.com"
-$bucketName = "silvaggiochristian-veeam-dtc"
-$folderName = "441"
+$repositoryName = Read-Host "Enter the repository name" | Out-String
+$accessKey = Read-Host "Enter the access key" | Out-String
+$secretKey = Read-Host "Enter the secret key" | Out-String
+$endpoint = Read-Host "Enter the S3 endpoint url" | Out-String
+$regionId = Read-Host "Enter the region ID" | Out-String
+$bucketName = Read-Host "Enter the bucket name" | Out-String
+$folderName = Read-Host "Enter the folder name" | Out-String
 
 # Add the S3 Account
 $account = Add-VBRAmazonAccount -AccessKey $accessKey -SecretKey $secretKey -Description $description
 
 # Create the S3 repository
-$connect = Connect-VBRAmazonS3CompatibleService -Account $account -CustomRegionId "us-west-2" -ServicePoint "https://s3.us-west-002.backblazeb2.com"
+$connect = Connect-VBRAmazonS3CompatibleService -Account $account -CustomRegionId "us-west-2" -ServicePoint $endpoint
 $bucket = Get-VBRAmazonS3Bucket -Connection $connect -Name $bucketName
 $folder = New-VBRAmazonS3Folder -Name $folderName -Connection $connect -Bucket $bucket
 Add-VBRAmazonS3CompatibleRepository -AmazonS3Folder $folder -Connection $connect -Name $repositoryName -EnableBackupImmutability -ImmutabilityPeriod
