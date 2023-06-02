635ad4d540a5ecd4d6344939f3936ea547056e5d
Nate Smith <nate.smith@dtctoday.com>
2023-06-02 13:53:39 -0400

    update

--- veeam/veeam-add-backup-repo.ps1
+++ veeam/veeam-add-backup-repo.ps1
@@ -1,7 +1,24 @@
 ## Please note this script can only support the following backup repository types ##
-# S3 Compabitble & Local
+# S3 Compabitble & Local. Both are forced.
 
-Start-Transcript 
+Start-Transcript -Path $env:WINDIR\logs\veeam-add-backup-repo.log
+
+Write-Host "Checking if we are running from a RMM or not."
+
+if ($rmm -ne 1) { 
+    # Set the repository details
+    # $repositoryType = "Please enter the repository type (1 S3 Compatible; 2 Windows Local; 3 Both)"
+    $description = Read-Host "Please enter the ticket # or project ticket # related to this configuration"
+    $immutabilityPeriod = Read-Host "Enter how many days every object is immutable for"
+    # $repositoryName = Read-Host "Enter the repository name" | Out-String
+    $accessKey = Read-Host "Enter the access key"
+    $secretKey = Read-Host "Enter the secret key"
+    $endpoint = Read-Host "Enter the S3 endpoint url"
+    $regionId = Read-Host "Enter the region ID"
+    $bucketName = Read-Host "Enter the bucket name"
+
+}
+
 # Make sure PSModulePath includes Veeam Console
 $MyModulePath = "C:\Program Files\Veeam\Backup and Replication\Console\"
 $env:PSModulePath = $env:PSModulePath + "$([System.IO.Path]::PathSeparator)$MyModulePath"
@@ -16,17 +33,6 @@ if ($Modules = Get-Module -ListAvailable -Name Veeam.Backup.PowerShell) {
 
 # Set Timestamp
 $timeStamp = [int](Get-Date -UFormat %s -Millisecond 0)
-
-# Set the repository details
-$repositoryType = "Please enter the repository type (1 S3 Compatible; 2 Windows Local; 3 Both)"
-$description = Read-Host "Please enter the ticket # or project ticket # related to this configuration"
-$immutabilityPeriod = Read-Host "Enter how many days every object is immutable for"
-# $repositoryName = Read-Host "Enter the repository name" | Out-String
-$accessKey = Read-Host "Enter the access key"
-$secretKey = Read-Host "Enter the secret key"
-$endpoint = Read-Host "Enter the S3 endpoint url"
-$regionId = Read-Host "Enter the region ID"
-$bucketName = Read-Host "Enter the bucket name"
 $folderName = $timeStamp
 
 # Add the S3 Account
@@ -42,17 +48,6 @@ Add-VBRAmazonS3CompatibleRepository -AmazonS3Folder $folder -Connection $connect
 
 # Display the added repository details
 $repository
-
-Stop-Transcript
-
-
-# Install Veeam PowerShell module if not already installed
-if (-not (Get-Module -ListAvailable -Name VeeamPSSnapin)) {
-    Install-Module -Name VeeamPSSnapin -Scope CurrentUser
-}
-
-# Import the Veeam PowerShell module
-Import-Module -Name VeeamPSSnapin
 
 # Get all logical drives on the system
 $drives = Get-WmiObject -Class Win32_LogicalDisk | Where-Object { $_.DriveType -eq 3 }
