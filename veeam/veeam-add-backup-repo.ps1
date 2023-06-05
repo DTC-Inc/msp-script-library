c181fab150d41bd7e44efcbd45b49ee4e0af4d67
Nate Smith <40123869+nettts@users.noreply.github.com>
2023-06-05 13:25:57 -0400

    Update veeam-add-backup-repo.ps1

--- veeam/veeam-add-backup-repo.ps1
+++ veeam/veeam-add-backup-repo.ps1
@@ -5,19 +5,19 @@ Start-Transcript -Path $env:WINDIR\logs\veeam-add-backup-repo.log
 
 Write-Host "Checking if we are running from a RMM or not."
 
-if ($rmm -ne 1) { 
+# if ($rmm -ne 1) { 
     # Set the repository details
     # $repositoryType = "Please enter the repository type (1 S3 Compatible; 2 Windows Local; 3 Both)"
-    $description = Read-Host "Please enter the ticket # or project ticket # related to this configuration"
-    $immutabilityPeriod = Read-Host "Enter how many days every object is immutable for"
+    # $description = Read-Host "Please enter the ticket # or project ticket # related to this configuration"
+    # $immutabilityPeriod = Read-Host "Enter how many days every object is immutable for"
     # $repositoryName = Read-Host "Enter the repository name" | Out-String
-    $accessKey = Read-Host "Enter the access key"
-    $secretKey = Read-Host "Enter the secret key"
-    $endpoint = Read-Host "Enter the S3 endpoint url"
-    $regionId = Read-Host "Enter the region ID"
-    $bucketName = Read-Host "Enter the bucket name"
+    # $accessKey = Read-Host "Enter the access key"
+    # $secretKey = Read-Host "Enter the secret key"
+    # $endpoint = Read-Host "Enter the S3 endpoint url"
+    # $regionId = Read-Host "Enter the region ID"
+    # $bucketName = Read-Host "Enter the bucket name"
 
-}
+# }
 
 # Make sure PSModulePath includes Veeam Console
 $MyModulePath = "C:\Program Files\Veeam\Backup and Replication\Console\"
