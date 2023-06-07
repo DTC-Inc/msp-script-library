f6e975c681b4105ba6e261578f85a9d95677a864
Nate Smith <nate.smith@dtctoday.com>
2023-06-06 23:08:33 -0400

    Revert "Merge branch 'main' of https://github.com/DTC-Inc/msp-script-library"
    This reverts commit fe140ca7a850add5f88a34c8607a9a615c7fc84d, reversing
    changes made to 935abbe11373a7fa2201603151ecc7be40bcee81.

--- veeam/veeam-get-backup-jobs.ps1
+++ veeam/veeam-get-backup-jobs.ps1
@@ -1,14 +1,3 @@
-# Install veeam powershell module
-if ($Modules = Get-Module -ListAvailable -Name Veeam.Backup.PowerShell) {
-    try {
-        $Modules | Import-Module -WarningAction SilentlyContinue
-        }
-        catch {
-            throw "Failed to load Veeam Modules"
-            }
- }
-
 # Get Backup Jobs
-Write-Host "Getting Backup Jobs"
-Get-VBRJob
+Get-VBRComputerBackupJob
 Get-VBRComputerBackupJob
