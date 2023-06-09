d0427c9d1645040ef00dd733d24cb7cef6183965
Nate Smith <nate.smith@dtctoday.com>
2023-06-09 12:24:02 -0400

    updates

--- iaas-backblaze/b2-create-buckets.ps1
+++ iaas-backblaze/b2-create-buckets.ps1
@@ -23,18 +23,16 @@ if (rmm -ne 1) {
           $clientList = Import-Csv $path | Select-Object -Skip 1 | ForEach-Object {
                $row = $_.PSObject.Properties.Value
                $cleanedRow = $row -replace '\W','' -replace ' ','-' | ForEach-Object { $_.ToLower() }
-               $bucketList = "$cleanedRow-veeam-dtc"
-          $bucketList
-      } else {
-          $clientList = Read-Host "Enter clients comma separated. Enter them exactly as is in your PSA" | ForEach-Object {
-               $row = $_.PSObject.Properties.Value
-               $cleanedRow = $row -replace '\W','' -replace ' ','-' | ForEach-Object { $_.ToLower()
                $bucketList = "veeam-dtc-$cleanedRow"
                $bucketList
           }
-          $userApiKey = Read-Host "Enter API Key ID"
-          $userApiSecret = Read-Host "Enter API App Key"
-          
+      } else {
+          $clientList = Read-Host "Enter clients comma separated. Enter them exactly as is in your PSA" | ForEach-Object {
+               $row = $_.PSObject.Properties.Value
+               $cleanedRow = $row -replace '\W','' -replace ' ','-' | ForEach-Object { $_.ToLower() }
+               $bucketList = "veeam-dtc-$cleanedRow"
+               $bucketList
+          }
      }
 
      # Create bucket for each client
@@ -46,14 +44,13 @@ if (rmm -ne 1) {
           Write-Host $client " " $keyOut
           $keyId, $keyApp = $keyOut -split '\s+'
           $data = [PSCustomObject]@{
-          BucketName = $client 
-          KeyId = $keyId
-          KeyApp = $keyApp
-     }
+               BucketName = $client 
+               KeyId = $keyId
+               KeyApp = $keyApp
+          }
 
      $data | Export-Csv -Path "$psScriptRoot\bucket-info.csv" -NoTypeInformation -Append
-
-}
+     }
 Stop-Transcript
 
 } else {
@@ -64,16 +61,9 @@ Stop-Transcript
      Write-Host "Access key: $userApiKey"
      Write-Host "Secret key: **redacted for sensativity**"
 
-     $binDir = Join-Path -Path $rmmScriptPath -ChildPath "bin"
-     $filePath = Join-Path -Path $binDir -ChildPath "backblaze-b2.exe"
+     $filePath = Join-Path -Path $rmmScriptPath -ChildPath "backblaze-b2.exe"
      
-     if (-not (Test-Path -Path $filePath)) {
-         if (-not (Test-Path -Path $binDir)) {
-             Write-Host "Creating Bin directory..."
-             New-Item -ItemType Directory -Path $binDir | Out-Null
-             Write-Host "Bin directory created."
-         }
-     
+     if (-not (Test-Path -Path $filePath)) {    
          $url = "https://github.com/DTC-Inc/msp-script-library/blob/main/iaas-backblaze/b2-windows.exe"  # Replace with the actual download URL
      
          Write-Host "Downloading file..."
@@ -84,12 +74,12 @@ Stop-Transcript
          Write-Host "File already exists."
      }
 
-
+     $bucketName = "$client-veeam-dtc"
      Write-Host "Creating bucket: $bucketName"
-     .\$psScriptRoot\b2-windows.exe authorize-account $userApiKey $userApiSecret
-     .\$psScriptRoot\b2-windows.exe create-bucket --defaultServerSideEncryptionAlgorithm "AES256" --defaultServerSideEncryption "SSE-B2" --fileLockEnabled $client "allPrivate" 
-     $keyOut = .\b2-windows.exe create-key $client "listAllBucketNames,listBuckets,readBuckets,readBucketEncryption,writeBucketEncryption, readBucketRetentions,writeBucketRetentions,listFiles,readFiles,shareFiles,writeFiles, deleteFiles,readFileLegalHolds,writeFileLegalHolds,readFileRetentions,writeFileRetentions,bypassGovernance" --bucket $client
-     Write-Host $client " " $keyOut
+     .\$rmmScriptPath\b2-windows.exe authorize-account $userApiKey $userApiSecret
+     .\$rmmScriptPath\b2-windows.exe create-bucket --defaultServerSideEncryptionAlgorithm "AES256" --defaultServerSideEncryption "SSE-B2" --fileLockEnabled $bucketName "allPrivate" 
+     $keyOut = .\$rmmScriptPath\b2-windows.exe create-key $bucketName "listAllBucketNames,listBuckets,readBuckets,readBucketEncryption,writeBucketEncryption, readBucketRetentions,writeBucketRetentions,listFiles,readFiles,shareFiles,writeFiles, deleteFiles,readFileLegalHolds,writeFileLegalHolds,readFileRetentions,writeFileRetentions,bypassGovernance" --bucket $bucketName
+     Write-Host $bucketName " " $keyOut
      $keyId, $keyApp = $keyOut -split '\s+'
      
      Write-Host "API Key ID: $keyId"
