988c1a7823428f8d8e34a5e29c41dca3a9aa0966
Nate Smith <40123869+nettts@users.noreply.github.com>
2023-08-22 13:23:51 -0400

    Create veeam-disable-objectStroage-immutability.ps1

---
+++ bdr-veeam/veeam-disable-objectStroage-immutability.ps1
@@ -0,0 +1,12 @@
+# Install veeam powershell module
+if ($Modules = Get-Module -ListAvailable -Name Veeam.Backup.PowerShell) {
+    try {
+        $Modules | Import-Module -WarningAction SilentlyContinue
+        }
+        catch {
+            throw "Failed to load Veeam Modules"
+            }
+ }
+
+$backupRepo = $backupRepo = Get-VBRBackupRepository |Where -Property Type -like "AmazonS3*"
+$backupRepo.DisableImmutability()
