4b3160bcdf5df2d522d600e1d63b8e8db850bc5c
Nate Smith <40123869+nettts@users.noreply.github.com>
2023-08-22 13:34:00 -0400

    Update

--- bdr-veeam/veeam-disable-objectStroage-immutability.ps1
+++ bdr-veeam/veeam-disable-objectStroage-immutability.ps1
@@ -1,3 +1,37 @@
+# This script disables all Amazon S3 and Amazon S3 compabitible object storage immutability.
+
+# Getting input from user if not running from RMM else set variables from RMM.
+
+Write-Host $description
+Write-Host $rmmScriptPath
+Write-Host $rmm
+
+$scriptLogName = "veeam-disable-objectStorage-immutability.log"
+
+if ($rmm -ne 1) {
+    $validInput = 0
+    # Only running if S3 Copy Job is true for this part.
+    while ($validInput -ne 1) {
+        # Ask for input here. This is the interactive area for getting variable information.
+        # Remember to make validInput = 1 whenever correct input is given.
+        $description = Read-Host "Please enter the ticket # and, or your initials. Its used as the description for the job"
+        $validInput = 1
+    }
+    $logPath = "$env:WINDIR\logs\$scriptLogName"
+
+
+} else { 
+    # Store the logs in the rmmScriptPath
+    $logPath = "$rmmScriptPath\logs\$scriptLogName"
+
+    if ($description -eq $null) {
+        Write-Host "Description is null. This was most likely run automatically from the RMM and no information was passed."
+        $description = "No description"
+    }   
+}
+
+Start-Transcript -Path $logPath
+
 # Install veeam powershell module
 if ($Modules = Get-Module -ListAvailable -Name Veeam.Backup.PowerShell) {
     try {
@@ -10,3 +44,5 @@ if ($Modules = Get-Module -ListAvailable -Name Veeam.Backup.PowerShell) {
 
 $backupRepo = $backupRepo = Get-VBRBackupRepository |Where -Property Type -like "AmazonS3*"
 $backupRepo.DisableImmutability()
+
+Stop-Transcript
--- script-template-powershell.ps1
+++ script-template-powershell.ps1
@@ -1,24 +1,31 @@
 # Getting input from user if not running from RMM else set variables from RMM.
 
-$scriptLogName = "enter-your-script-log-name-here.log"
+Write-Host $description
+Write-Host $rmmScriptPath
+Write-Host $rmm
+
+$scriptLogName = "Put the log file name here."
 
 if ($rmm -ne 1) {
     $validInput = 0
-    # Only running if S3 Copy Job is true for this part.
+    # Checking for valid input.
     while ($validInput -ne 1) {
         # Ask for input here. This is the interactive area for getting variable information.
         # Remember to make validInput = 1 whenever correct input is given.
-
+        $description = Read-Host "Please enter the ticket # and, or your initials. Its used as the description for the job"
+        $validInput = 1
     }
     $logPath = "$env:WINDIR\logs\$scriptLogName"
-    $description = Read-Host "Please enter the ticket # and, or your initials. Its used as the description for the job"
 
 
 } else { 
     # Store the logs in the rmmScriptPath
     $logPath = "$rmmScriptPath\logs\$scriptLogName"
-    
 
+    if ($description -eq $null) {
+        Write-Host "Description is null. This was most likely run automatically from the RMM and no information was passed."
+        $description = "No description"
+    }   
 }
 
 Start-Transcript -Path $logPath
