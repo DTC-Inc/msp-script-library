43e5965848ece27cb2ac5a633d996725cd648d83
Nate Smith <40123869+nettts@users.noreply.github.com>
2024-03-18 20:04:08 -0400

    Update cwcontrol-uninstall.ps1

--- rmm-cwcontrol/cwcontrol-uninstall.ps1
+++ rmm-cwcontrol/cwcontrol-uninstall.ps1
@@ -55,7 +55,8 @@ if ($Services) {
                 $Service | Stop-Service -Force -ErrorAction SilentlyContinue
                 $ServiceDeleteResult = sc.exe delete "$Service.Name"
                 # $ServiceDeleteResult | Wait-Process -Timeout 10
-                if ($ServiceDeleteResult -eq 0) {
+                $IsServiceDeleted = Get-Service | Where-Object { $_.Name -like $Service.Name
+                if (!($IsServiceDeleted)) {                    
                     Write-Host "Service $($Service.Name) forcibly deleted."
                     Exit 0
                 } else {
