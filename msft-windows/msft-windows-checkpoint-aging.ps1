2d75f12afacd0c567de9379a8ccb15405827c31e
Nate Smith <40123869+nettts@users.noreply.github.com>
2024-03-29 09:50:42 -0400

    Update msft-windows-checkpoint-aging.ps1

--- msft-windows/msft-windows-checkpoint-aging.ps1
+++ msft-windows/msft-windows-checkpoint-aging.ps1
@@ -1,6 +1,6 @@
 # Getting input from user if not running from RMM else set variables from RMM.
 
-$ScriptLogName = "EnterLogNameHere.log"
+$ScriptLogName = "windows-hyper-v-checkpoint-aging.log"
 
 if ($RMM -ne 1) {
     $ValidInput = 0
@@ -9,6 +9,7 @@ if ($RMM -ne 1) {
         # Ask for input here. This is the interactive area for getting variable information.
         # Remember to make ValidInput = 1 whenever correct input is given.
         $Description = Read-Host "Please enter the ticket # and, or your initials. Its used as the Description for the job"
+        $DaysAging = Read-Host "Please enter the amount of days to consider a Hyper-V"
         if ($Description) {
             $ValidInput = 1
         } else {
@@ -28,7 +29,7 @@ if ($RMM -ne 1) {
     }
 
     if ($null -eq $Description) {
-        Write-Host "Description is null. This was most likely run automatically from the RMM and no information was passed."
+        Write-Output "Description is null. This was most likely run automatically from the RMM and no information was passed."
         $Description = "No Description"
     }   
 
@@ -38,8 +39,19 @@ if ($RMM -ne 1) {
 
 Start-Transcript -Path $LogPath
 
-Write-Host "Description: $Description"
-Write-Host "Log path: $LogPath"
-Write-Host "RMM: $RMM"
+Write-Output "Description: $Description"
+Write-Output "Log path: $LogPath"
+Write-Output "RMM: $RMM"
+Write-Output "Days Aging: $DaysAging"
+
+$AgingCheckpoints = Get-Vm | Get-VMSnapshot | Where {$_.CreationTime -lt (Get-Date).AddDays($DaysAging)}
+
+if ($AgingCheckpoints) { 
+    $AgingCheckpints | ForEach-Object { Write-Output "Checkpint $_.Name is older than 1 day. Please delete this checkpoint."}
+    Exit 1
+} else {
+    Write-Output "There are no checkpints older than 1 day that need deleted."
+    Exit 0
+}
 
 Stop-Transcript
