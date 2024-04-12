2d098ab348b16bfb2e3c6251f007c4e79391bd9a
Nate Smith <40123869+nettts@users.noreply.github.com>
2024-04-12 16:59:36 -0400

    Update script-template-powershell.ps1

--- script-template-powershell.ps1
+++ script-template-powershell.ps1
@@ -1,3 +1,6 @@
+## PLEASE COMMENT YOUR VARIALBES DIRECTLY BELOW HERE IF YOU'RE RUNNING FROM A RMM
+## THIS IS HOW WE EASILY LET PEOPLE KNOW WHAT VARIABLES NEED SET IN THE RMM
+
 # Getting input from user if not running from RMM else set variables from RMM.
 
 $ScriptLogName = "EnterLogNameHere.log"
