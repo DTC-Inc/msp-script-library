cc1eafee54b8c2504bb375a80be0189676534816
Nate Smith <40123869+nettts@users.noreply.github.com>
2024-06-28 14:08:49 -0400

    Update teamviewer-uninstall.ps1

--- rmm-misc/teamviewer-uninstall.ps1
+++ rmm-misc/teamviewer-uninstall.ps1
@@ -62,34 +62,50 @@ if ($service) {
     Write-Output "TeamViewer service not found."
 }
 
-# Define the TeamViewer uninstall key
-$uninstallKey64bit = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall"
-$uninstallKey32bit = "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall"
+$test64Bit = Test-Path $ENV:PROGRAMFILES\TeamViewer\uninstall.exe -PathType Leaf
+$test32Bit = Test-Path $ENV:PROGRAMFILES(X86)\TeamViewer\uninstall.exe -PathType Leaf
+if ($test64Bit) {
 
-if ($uninstallKey64bit) {
-    $teamViewerKey = Get-ChildItem -Path $uninstallKey64bit | Where-Object { $_.GetValue("DisplayName") -like "*TeamViewer*" }
-} else { 
-    $teamViewerKey = Get-ChildItem -Path $uninstallKey32bit | Where-Object { $_.GetValue("DisplayName") -like "*TeamViewer*" }
+    & $ENV:PROGRAMFILES\TeamViewer\uninstall.exe /S
 }
+
+if ($test32Bit) {
+    & $ENV:PROGRAMFILES(X86)\TeamViewer\uninstall.exe /S
+
+}
+
+
+
+$osArchitecture = (Get-CimInstance Win32_operatingsystem).OSArchitecture
+
+# Define the TeamViewer uninstall key
+#$uninstallKey64bit = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall"
+#$uninstallKey32bit = "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall"
+
+#if ($uninstallKey64bit) {
+    #$teamViewerKey = Get-ChildItem -Path $uninstallKey64bit | Where-Object { $_.GetValue("DisplayName") -like "*TeamViewer*" }
+#} else { 
+    #$teamViewerKey = Get-ChildItem -Path $uninstallKey32bit | Where-Object { $_.GetValue("DisplayName") -like "*TeamViewer*" }
+#}
 
 # Check if TeamViewer is installed
-if ($teamViewerKey) {
-    Write-Output "TeamViewer is installed. Uninstalling..."
+#if ($teamViewerKey) {
+    #Write-Output "TeamViewer is installed. Uninstalling..."
     
     # Get the uninstall string
-    $uninstallString = $teamViewerKey.GetValue("UninstallString")
+    #$uninstallString = $teamViewerKey.GetValue("UninstallString")
     
     # Remove quotes from the uninstall string if they exist
-    $uninstallString = $uninstallString -replace '"', ''
+    #$uninstallString = $uninstallString -replace '"', ''
     
     # Execute the uninstall string
-    Start-Process -FilePath $uninstallString -ArgumentList "/S" -Wait
+    #Start-Process -FilePath $uninstallString -ArgumentList "/S" -Wait
     
-    Write-Output "TeamViewer has been uninstalled."
-} else {
-    Write-Output "TeamViewer is not installed."
-}
+    #Write-Output "TeamViewer has been uninstalled."
+#} else {
+    #Write-Output "TeamViewer is not installed."
+#}
 
 
 
-Stop-Transcript
+#Stop-Transcript
