48f981fe77500b95e9c82fae0ce5ba3b273bfffc
Nate Smith <40123869+Gumbees@users.noreply.github.com>
2025-05-12 08:42:26 -0400

    Update tsprint-client-install.ps1

--- app-tsprint/tsprint-client-install.ps1
+++ app-tsprint/tsprint-client-install.ps1
@@ -3,7 +3,7 @@
 
 # Getting input from user if not running from RMM else set variables from RMM.
 
-$ScriptLogName = "EnterLogNameHere.log"
+$ScriptLogName = "tsprint-install.log"
 
 if ($RMM -ne 1) {
     $ValidInput = 0
