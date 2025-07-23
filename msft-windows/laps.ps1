5e3a827fd9889083f5755a780c1bb44231541d72
scottlei4dtctoday <131402416+scottlei4dtctoday@users.noreply.github.com>
2025-07-23 13:44:00 -0400

    Update laps.ps1

--- msft-windows/laps.ps1
+++ msft-windows/laps.ps1
@@ -72,30 +72,31 @@ if ($serverRole.DomainRole -eq 4 -or $serverRole.DomainRole -eq 5) {
     Write-Host "Continuing to run LAPS."
 }
 
+# Function to check if azure ad joined
 function Test-AzureAdJoined {
-    $AzureADKey = Test-Path "HKLM:/SYSTEM/CurrentControlSet/Control/CloudDomainJoin/JoinInfo"
-    if ($AzureADKey) {
-        $subKey = Get-Item "HKLM:/SYSTEM/CurrentControlSet/Control/CloudDomainJoin/JoinInfo/*"
+        $AzureADKey = Test-Path "HKLM:/SYSTEM/CurrentControlSet/Control/CloudDomainJoin/JoinInfo"
+        if ($AzureADKey) {
+            $subKey = Get-Item "HKLM:/SYSTEM/CurrentControlSet/Control/CloudDomainJoin/JoinInfo/*"
+    
+            try {
+                foreach($key in $subKey) {
+                    $tenantId = $key.GetValue("TenantId");
+                    $userEmail = $key.GetValue("UserEmail");
+                }
 
-        try {
-            foreach ($key in $subKey) {
-                $tenantId = $key.GetValue("TenantId")
-                $userEmail = $key.GetValue("UserEmail")
-            }
-
-            Write-Host "Tenant ID: $($tenantId)" 
-            Write-Host "User Email: $($userEmail)"
-            if ($tenantId) { 
-                return $True
-            } else {
+                Write-Host "Tenant ID: $($tenantId)" 
+                Write-Host "User Email: $($userEmail)"
+                if ($tenantId) { 
+                    return $True
+                } else {
+                    return $False
+                }
+            } catch {
                 return $False
             }
-        } catch {
-            return $False
+        } else {
+                return $False
         }
-    } else {
-        return $False
-    }
 }
 
 # Function to generate a user-friendly random password
